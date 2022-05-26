92d9e6418ffb49f3a7fa0f3dff6593fd8c79fcdc
Robert Szacki <robert.szacki@gmail.com>
2022-05-26 11:16:32 +0200

    Added IFF and ILBM support.

---
+++ Src/Init/IFF/IFF.c
@@ -0,0 +1,70 @@
+
+/*
+** GameX engine
+** Src > Init > IFF
+*/
+
+struct IFFHandle *openIFF(STRPTR name, LONG mode)
+{
+    struct IFFHandle *iff;
+    LONG dosModes[] =
+    {
+        MODE_OLDFILE,
+        MODE_NEWFILE
+    };
+
+    if (iff = AllocIFF())
+    {
+        if (iff->iff_Stream = Open(name, dosModes[mode]))
+        {
+            InitIFFasDOS(iff);
+            if (OpenIFF(iff, mode) == 0)
+            {
+                return(iff);
+            }
+            Close(iff->iff_Stream);
+        }
+        FreeIFF(iff);
+    }
+    return(NULL);
+}
+
+VOID closeIFF(struct IFFHandle *iff)
+{
+    CloseIFF(iff);
+    Close(iff->iff_Stream);
+    FreeIFF(iff);
+}
+
+BYTE *findPropData(struct IFFHandle *iff, ULONG type, ULONG ID)
+{
+    struct StoredProperty *sp;
+    
+    if (sp = FindProp(iff, type, ID))
+    {
+        return(sp->sp_Data);
+    }
+    return(NULL);
+}
+
+BYTE *getChunkBuffer(struct IFFHandle *iff, LONG *pSize)
+{
+    struct ContextNode *cn;
+
+    if (cn = CurrentChunk(iff))
+    {
+        LONG size = cn->cn_Size;
+        BYTE *buffer;
+
+        if (buffer = AllocMem(size, MEMF_PUBLIC))
+        {
+            if (ReadChunkBytes(iff, buffer, size) == size)
+            {
+                *pSize = size;
+                return(buffer);
+            }
+            FreeMem(buffer, size);
+        }
+    }
+    return(NULL);
+}
---
+++ Src/Init/IFF/ILBM.c
@@ -0,0 +1,201 @@
+
+/*
+** GameX engine
+** Src > Init > IFF
+*/
+
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <datatypes/pictureclass.h>
+#include <libraries/iffparse.h>
+
+#include <clib/exec_protos.h>
+#include <clib/iffparse_protos.h>
+#include <clib/graphics_protos.h>
+
+#define RGB(c) ((c)|((c)<<8)|((c)<<16)|((c)<<24))
+#define RowBytes(w) ((((w) + 15) >> 4) << 1)
+
+BOOL scanILBM(struct IFFHandle *iff)
+{
+    if (PropChunk(iff, ID_ILBM, ID_BMHD) == 0)
+    {
+        if (PropChunk(iff, ID_ILBM, ID_CMAP) == 0)
+        {
+            if (PropChunk(iff, ID_ILBM, ID_CAMG) == 0)
+            {
+                if (StopChunk(iff, ID_ILBM, ID_BODY) == 0)
+                {
+                    if (ParseIFF(iff, IFFPARSE_SCAN) == 0)
+                    {
+                        return(TRUE);
+                    }
+                }
+            }
+        }
+    }
+    return(FALSE);
+}
+
+VOID setCol(ULONG *pal, WORD c, UBYTE red, UBYTE green, UBYTE blue)
+{
+    pal[(c * 3) + 1] = RGB(red);
+    pal[(c * 3) + 2] = RGB(green);
+    pal[(c * 3) + 3] = RGB(blue);
+}
+
+ULONG *getPal(struct IFFHandle *iff)
+{
+    struct StoredProperty *sp;
+
+    if (sp = FindProp(iff, ID_ILBM, ID_CMAP))
+    {
+        LONG size = sp->sp_Size;
+        UBYTE *cmap = sp->sp_Data;
+        WORD count = size / 3;
+        ULONG *pal;
+        
+        if (pal = AllocVec((size + 2) * sizeof(ULONG), MEMF_PUBLIC))
+        {
+            WORD c;
+            pal[0] = count << 16;
+            for (c = 0; c < count; c++)
+            {
+                setCol(pal, c, cmap[0], cmap[1], cmap[2]);
+                cmap += 3;
+            }
+            pal[size + 1] = 0;
+            return(pal);
+        }        
+    }
+    return(NULL);
+}
+
+BOOL unpackRow(BYTE **pSrc, BYTE *dest, LONG *pSize, WORD bpr, UBYTE cmp)
+{
+    BYTE *src = *pSrc;
+    LONG size = *pSize;
+
+    if (cmp == cmpNone)
+    {
+        if (size < bpr)
+        {
+            return(FALSE);
+        }
+        CopyMem(src, dest, bpr);
+        src += bpr;
+        size -= bpr;
+    }
+    else if (cmp == cmpByteRun1)
+    {
+        while (bpr > 0)
+        {
+            BYTE c;
+            if (size < 1)
+            {
+                return(FALSE);
+            }
+            size--;
+            if ((c = *src++) >= 0)
+            {
+                WORD count = c + 1;
+                if (size < count || bpr < count)
+                {
+                    return(FALSE);                    
+                }
+                size -= count;
+                bpr -= count;
+                while (count-- > 0)
+                {
+                    *dest++ = *src++;
+                }
+            }
+            else if (c != -128)
+            {
+                WORD count = (-c) + 1;
+                BYTE data;
+                if (size < 1 || bpr < count)
+                {
+                    return(FALSE);
+                }
+                size--;
+                bpr -= count;
+                data = *src++;
+                while (count-- > 0)
+                {
+                    *dest++ = data;
+                }
+            }
+        }
+    }
+    else
+    {
+        return(FALSE);
+    }
+
+    *pSrc = src;    
+    *pSize = size;
+    return(TRUE);
+}
+
+struct BitMap *unpackBitMap(struct IFFHandle *iff)
+{
+    struct BitMap *bm;
+    struct BitMapHeader *bmhd;
+
+    if (bmhd = (struct BitMapHeader *)findPropData(iff, ID_ILBM, ID_BMHD))
+    {
+        WORD width = bmhd->bmh_Width, height = bmhd->bmh_Height;
+        UBYTE depth = bmhd->bmh_Depth;
+        UBYTE cmp = bmhd->bmh_Compression;
+        WORD bpr = RowBytes(width);
+
+        if (bmhd->bmh_Masking == mskHasMask)
+        {
+            ++depth;
+        }
+
+        if (bm = AllocBitMap(width, height, depth, BMF_INTERLEAVED, NULL))
+        {
+            BYTE *buffer;
+            LONG size;
+
+            if (buffer = getChunkBuffer(iff, &size))
+            {
+                WORD row, plane;
+                PLANEPTR planes[8 + 1];
+                BYTE *curBuf = buffer;
+                LONG curSize = size;
+                BOOL success = FALSE;
+
+                for (plane = 0; plane < depth; plane++)
+                {
+                    planes[plane] = bm->Planes[plane];
+                }
+
+                for (row = 0; row < height; row++)
+                {
+                    for (plane = 0; plane < depth; plane++)
+                    {
+                        if (!(success = unpackRow(&curBuf, planes[plane], &curSize, bpr, cmp)))
+                        {
+                            break;
+                        }
+                        planes[plane] += bm->BytesPerRow;
+                    }
+                    if (!success)
+                    {
+                        break;
+                    }
+                }
+                FreeMem(buffer, size);
+                if (success)
+                {
+                    return(bm);
+                }
+            }
+            FreeBitMap(bm);
+        }
+    }
+    return(NULL);
+}
